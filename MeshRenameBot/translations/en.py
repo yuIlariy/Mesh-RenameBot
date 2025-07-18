75ce6da68ab301ff9c40b4bcb849dd5a2c1566c5
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-18 21:51:30 +0300

    Update en.py

--- MeshRenameBot/translations/en.py
+++ MeshRenameBot/translations/en.py
@@ -9,9 +9,10 @@ class EnglishTranslations:
     WRONG_VALUE_ERROR = "❌ Invalid value entered for {{ variable_name }} variable."
 
     START_MSG = (
-        "👋 Hello there! I am **Mesh Rename Bot**.\n\n"
+        "👋 Hello there! I am **Auto Rename Bot**.\n\n"
         "🌟 Open Source: [GitHub](https://github.com/yash-dk/Mesh-RenameBot/tree/master)\n"
         "🚀 You can deploy your own instance!"
+        "🚀 **Powered by [NAm](https://t.me/xspes)\n"
     )
 
     CANCEL_MESSAGE = "⚠️ The rename has been **canceled**. Will be updated soon."
