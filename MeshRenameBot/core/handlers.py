d72295c46334fb82007417e8c84f4cdcf46436b2
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-22 13:13:02 +0300

    Update handlers.py

--- MeshRenameBot/core/handlers.py
+++ MeshRenameBot/core/handlers.py
@@ -117,6 +117,9 @@ def add_handlers(client: MeshRenameBot) -> None:
     client.add_handler(
     MessageHandler(status_handler, filters.regex(r"^/status$", re.IGNORECASE))
     )
+    
+    signal.signal(signal.SIGINT, term_handler)
+    signal.signal(signal.SIGTERM, term_handler)
 
 BOT_START_TIME = time.time()
 
@@ -142,10 +145,6 @@ async def status_handler(client: Client, msg: Message) -> None:
     )
     await msg.reply(status_text)
     
-
-    signal.signal(signal.SIGINT, term_handler)
-    signal.signal(signal.SIGTERM, term_handler)
-
 
 async def start_handler(_: MeshRenameBot, msg: Message) -> None:
     user_locale = UserDB().get_var("locale", msg.from_user.id)
